855ec75fb082fb7144305045887aba941f72eca5
raphaelws <44216544+raphaelws@users.noreply.github.com>
2020-03-31 13:53:18 -0300

    initial function changed
    initial function changed to groud state of harmonic oscilator

--- sch-cn.py
+++ sch-cn.py
@@ -3,25 +3,29 @@ import matplotlib.pyplot as plt
 import matplotlib
 from scipy import sparse as sparse
 from scipy.sparse import linalg as ln
+import math as mt
 
 matplotlib.rc('font', size=18)
 matplotlib.rc('font', family='Arial')
 
-m=0.0001 #massa
+m=1 #massa
 w=1 #frequencia
+e=2.718281828459045235360
 
-N = 31 #numero de pontos na malha
+
+N = 101 #numero de pontos na malha
 sigma = 1j #fazendo h=1 
 dt = 0.002 #delta tempo
-L = float(1) #tamanho da malha
+L = float(10) #tamanho da malha
 nsteps = 300 #numero de passos
 dx = L/(N-1) #espacamento calculado com base no numero de pontos
 nplot = 5 #fazer uma imagem a cada nplots passos
 pi=np.pi
+l0=np.sqrt(1/(m*w))
 
 #malha
 
-x = np.linspace(0,1,N)
+x = np.linspace(0,L,N)
 poten = m*w*x**2/2  #m w^2 x^2
 r = sigma*dt/dx**2 
 h_diag = np.ones(N) / dx**2 + poten
@@ -33,7 +37,7 @@ explicit = (sparse.eye(N) + dt / 2.0j * hamiltonian).tocsc()
 evolution_matrix = ln.inv(implicit).dot(explicit).tocsr()
  
 #consicoes iniciais 
-psi = np.asarray([np.sin(np.pi*xx) for xx in x])
+psi = np.asarray([e**(-0.5*(xx-0.5*L)**2/l0**2) for xx in x])
 
 
 prob = abs(psi)**2
@@ -42,9 +46,10 @@ prob /= norm
 psi /= norm**0.5
 
 
-ymax=[0,max(prob)+max(prob)*0.2]
+ymax=[-max(psi)-max(psi)*0.3,max(psi)+max(psi)*0.2]
 
-#plt.plot(x,psi,linewidth=2,label="PSI")
+
+plt.plot(x,psi,linewidth=2,label="PSI")
 plt.plot(x,prob,linewidth=2,label="|PSI|²")
 plt.ylim(ymax)
 plt.xlabel("x")
@@ -67,7 +72,7 @@ for j in range(nsteps):
     psi /= norm**0.5
 
     if(j%nplot==0): 
-        #plt.plot(x,psi,linewidth=2,label="PSI")
+        plt.plot(x,psi,linewidth=2,label="PSI")
         plt.plot(x,prob,linewidth=2,label="|PSI|²")
         plt.ylim(ymax)
         filename = 'harmonico' + str(k+1).zfill(3) + '.jpg';
